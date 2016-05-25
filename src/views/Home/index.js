bb6b20b27d7e201297169f5e4d1543ed413145f5
Cassandra Wilcox <csndra0504@gmail.com>
2016-05-25 13:01:02 -0400

    feat: homepage interest form

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -9,18 +9,39 @@
 
     $scope.init = function() {
       console.log('Hello HomeCtrl')
+      $scope.downloadCTA = 'Get the Beta';
     };
 
-    $scope.clickReql = function(event){
+    $scope.download = function(downloadForm){
+      downloadForm.$setDirty();
+
+      console.log("downloadForm.$valid", downloadForm.$valid)
+      if (downloadForm.$valid) {
+        analytics.alias($scope.downloadEmail, {}, {}, function() {
+          analytics.identify($scope.downloadEmail, {
+            email: $scope.downloadEmail
+          });
+
+          analytics.track('clicked get Ahhhgenda', {
+            "downloadCTA": $scope.downloadCTA,
+            "downloadEmail": $scope.downloadEmail
+          });
+
+          $timeout(function() {
+            $scope.showDownloadSuccess = true;
+            console.log("$scope.showDownloadSuccess", $scope.showDownloadSuccess)
+          });
+        })
+      }
       // console.log('event',event.target.innerHTML);
 
-      analytics.track('clicked get ReQL', {
-        "button cta": event.target.innerHTML,
-        "button link": null
-      });
+      // analytics.track('clicked get ReQL', {
+      //   "button cta": event.target.innerHTML,
+      //   "button link": null
+      // });
 
       // alert('Stay tuned! ReQL Pro will be available for download soon!');
-      $location.path('/rethink');
+      // $location.path('/rethink');
     }
 
 
